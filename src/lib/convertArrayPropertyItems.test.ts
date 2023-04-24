4d7edd0d34f0f1ad4a89012cbcb45a3a58464413
Francesco Iesu <francesco.iesu@deepcrawl.com>
2023-04-24 12:47:15 +0100

    chore: update tests

---
+++ src/lib/convertArrayPropertyItems.test.ts
@@ -0,0 +1,16 @@
+import assert from 'assert';
+import { convertArrayPropertyItems } from './convertArrayPropertyItems';
+
+describe('convertArrayPropertyItems', function () {
+  context('no Avro items equivalent', function () {
+    it('returns undefined when items is undefined', function () {
+      const result = convertArrayPropertyItems(true, []);
+      assert.strictEqual(result, undefined);
+    });
+
+    it('returns undefined when items is boolean', function () {
+      const result = convertArrayPropertyItems(true, []);
+      assert.strictEqual(result, undefined);
+    });
+  });
+});
